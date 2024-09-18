ca93d377f509df51339c525c11206ef1ca94fce1
yoannet <yoadiaval@alu.edu.gva.es>
2024-09-18 18:28:41 +0200

    update

---
+++ DWES/proyectos/bienvenidos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de bienvenida</title>
+</head>
+<body>
+ <h1>Página de bienvenida</h1>
+    <?php
+    echo("<p>Bienvenido a esta página</p>")
+    ?>
+</body>
+</html>
