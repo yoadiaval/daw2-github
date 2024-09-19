12f9ec6abe2410dd67d6ea2c82bd30a2e427674d
yoannet <yoadiaval@alu.edu.gva.es>
2024-09-19 18:20:02 +0200

    save

---
+++ DWES/proyectos/ejercicio1/info_basica.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio Info básica</title>
+</head>
+<body>
+    <h1>Ejercicio 1 Info  básica</h1>
+    <?php
+    $miNombre = "yoannet";
+    $anyoNac = "1993";
+    ?>
+
+    <p>Me llamo <?php echo $miNombre; ?> y nací en el año <?php echo  $anyoNac; ?></p>
+
+</body>
+</html>
