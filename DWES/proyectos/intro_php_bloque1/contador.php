62da7bdb5713ffe294c4ac4ffcf3596655bbe0f4
yoannet <yoadiaval@alu.edu.gva.es>
2024-09-23 18:18:37 +0200

    save

--- DWES/proyectos/intro_php_bloque1/contador.php
+++ DWES/proyectos/intro_php_bloque1/contador.php
@@ -3,28 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contador</title>
 </head>
 <body>
+    <h1>Ejercicio de contadores</h1>
+    <p>Este contador va del 1 al 100</p>
     <?php
     for($i = 1; $i <= 100; $i++){
         
-        if($i==100){echo $i;}else{
+        if($i==100){
+            echo $i;
+        }else{
             echo $i .", ";
         }
     }
 echo "<br/>";
-    $i= 10;
-    while($i >= 0){
-        if($i == 0){
-            echo $i;
-        }else{
-            echo $i . "-";
-        }
-        $i--;
-    }
 
+     ?>
+     <p>Este contador va del 10 al 0</p>
 
-    ?>
-</body>
+<?php
+   $i= 10;
+   while($i >= 0){
+       if($i == 0){
+           echo $i;
+       }else{
+           echo $i . "-";
+       }
+       $i--;
+   }
+
+?>
+    </body>
 </html>
