c54941459911bf17bf87eff610f6058a24122b8d
yoannet <yoadiaval@alu.edu.gva.es>
2024-09-24 17:35:01 +0200

    save

---
+++ DWES/proyectos/intro_php_funciones/contador_funciones.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function cuenta($a, $b){
+        if($a > $b){
+            for($i = $a; $i >= $b; $i--){
+                echo $i;
+                if($i != $b){
+                    echo ", ";
+                }
+            }
+        }else{
+            for($i = $a; $i <= $b; $i++){
+                echo $i;
+                if($i != $b){
+                    echo ", ";
+                }
+            }
+        }
+    }
+
+
+    cuenta(40,20);
+    ?>
+</body>
+</html>
---
+++ DWES/proyectos/intro_php_funciones/contador_funciones_ampliacion.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function cuenta(){
+
+        $a = func_get_arg(0);
+        $b = func_get_arg(1);
+         
+        if(func_num_args() == 3){
+            $salto = func_get_arg(2);
+         }else{
+            $salto = 1;
+         }
+
+if($a > $b){
+    $inicio = $a;
+    $fin = $b;
+    $saldo *= -1;
+}else{
+    $inicio = $b;
+    $fin = $a;
+}
+
+
+ while($inicio != $fin ){
+    echo $inicio;
+    if($inicio != $fin){
+        echo ", ";
+    }
+    $inicio += $salto;
+ }
+
+
+
+       /* if($a > $b){
+            for($i = $a; $i >= $b; $i -= $salto ){
+                echo $i;
+                if($i != $b){
+                    echo ", ";
+                }
+            }
+        }else{
+            for($i = $a; $i <= $b; $i += $salto){
+                echo $i;
+                if($i != $b){
+                    echo ", ";
+                }
+            }
+        }*/
+    }
+
+
+    cuenta(40,30,2);
+    ?>
+</body>
+</html>
