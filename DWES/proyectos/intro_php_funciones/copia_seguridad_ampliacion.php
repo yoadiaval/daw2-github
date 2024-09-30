7fc68677d2e42252b83126766b8427e8f7a617a3
yoannet <yoadiaval@alu.edu.gva.es>
2024-09-30 18:29:26 +0200

    save

--- DWES/proyectos/intro_php_funciones/copia_seguridad_ampliacion.php
+++ DWES/proyectos/intro_php_funciones/copia_seguridad_ampliacion.php
@@ -5,7 +5,6 @@ if(file_exists($fichero)){
     $content = file_get_contents($fichero);
     $patron = "/\w{1,}@\w{1,}\.[a-z]{2,4}/";
     $resultado = preg_replace($patron, "ydiaval@alu.edu.gva",$content);
-  
     echo $resultado;
 } 
 ?>
