61aa51a9dc8a0f7e40de0108d519a9b9c41b34df
yoadiaval <yoannet93@gmail.com>
2024-10-01 12:59:53 +0200

    update

--- DWES/proyectos/intro_php_funciones/ahora_ampliado.php
+++ DWES/proyectos/intro_php_funciones/ahora_ampliado.php
@@ -1,3 +1,7 @@
 //ampliación
 //crea un documento que , pasándole una fecha de nacimiento, te diga cuantos años, meses y dias tienes
 //comprueba que la fecha de nacimiento es correcta.
+<?php
+$fechaNac = "01/18/93";
+echo checkdate($fechaNac)
+?>
