5eadd3cc4d6454c0083361e50a1f9d52efecfd93
yoannet <yoadiaval@alu.edu.gva.es>
2024-10-01 15:52:26 +0200

    update

--- DWES/proyectos/intro_php_funciones/ahora_ampliado.php
+++ DWES/proyectos/intro_php_funciones/ahora_ampliado.php
@@ -10,9 +10,9 @@
 $fechaNac = "01/18/1993";
 $arrFechNac = explode("/", $fechaNac);
 
-// Validar si la fecha es correcta
+
 if (checkdate($arrFechNac[0], $arrFechNac[1], $arrFechNac[2])==1) {
-    // Convertir la fecha de nacimiento en timestamp
+   
     $nacToTime = strtotime("{$arrFechNac[2]}-{$arrFechNac[0]}-{$arrFechNac[1]}");
     $hoy = time();
     $edad = ($hoy - $nacToTime) / (60 * 60 * 24 * 365);
