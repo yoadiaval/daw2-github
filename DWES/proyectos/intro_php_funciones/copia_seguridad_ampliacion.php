ecb9a5c2a03d5ce1ea0134536c462c468555eb50
yoannet <yoadiaval@alu.edu.gva.es>
2024-10-01 15:51:56 +0200

    update

--- DWES/proyectos/intro_php_funciones/copia_seguridad_ampliacion.php
+++ DWES/proyectos/intro_php_funciones/copia_seguridad_ampliacion.php
@@ -5,6 +5,7 @@ if(file_exists($fichero)){
     $content = file_get_contents($fichero);
     $patron = "/\w{1,}@\w{1,}\.[a-z]{2,4}/";
     $resultado = preg_replace($patron, "ydiaval@alu.edu.gva",$content);
+    file_put_contents("datosCopia.txt", $resultado );
     echo $resultado;
 } 
 ?>
