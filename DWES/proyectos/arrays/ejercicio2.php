6b0df5cec27e7c7159124f75065b20f527e8c18c
yoannet <yoadiaval@alu.edu.gva.es>
2024-10-01 21:03:57 +0200

    update

--- DWES/proyectos/arrays/ejercicio2.php
+++ DWES/proyectos/arrays/ejercicio2.php
@@ -6,11 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-   <?php    $coches = array("111BCE"=>array("audi", "U", "4"), "111BCF"=>array("bmw", "A", "4"), "111BCG"=>array("chevrolet", "L", "2"), "111BCG"=>array("citroen", "F", "4"));
+   <?php    
+   $coches = array("111BCE"=>array("audi", "U", "4"), "111BCF"=>array("bmw", "A", "4"), "111BCG"=>array("chevrolet", "L", "2"), "111BCG"=>array("citroen", "F", "4"));
     for($coches as $coche){
         print_r($coche);
     }
     ?>
 
-</body>
+</body> 
 </html>
