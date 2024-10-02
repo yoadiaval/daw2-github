369a171e9becc6085b523da3e9474e55a22a9013
Yoannet Díaz Valdés <ydiaval@upv.edu.es>
2024-10-02 09:14:20 +0200

    update

--- DWES/proyectos/intro_php_funciones/ahora_ampliado.php
+++ DWES/proyectos/intro_php_funciones/ahora_ampliado.php
@@ -2,6 +2,8 @@
 //crea un documento que , pasándole una fecha de nacimiento, te diga cuantos años, meses y dias tienes
 //comprueba que la fecha de nacimiento es correcta.
 <?php
+$fechaActual = time();
+
 $fechaNac = "01/18/93";
 echo checkdate($fechaNac)
 ?>
