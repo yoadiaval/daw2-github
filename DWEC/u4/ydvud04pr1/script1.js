eafb3a3d830285d7c1f3d27df94289d8829839f0
yoannet <yoadiaval@alu.edu.gva.es>
2024-10-16 18:27:11 +0200

    update

--- DWEC/u4/ydvud04pr1/script1.js
+++ DWEC/u4/ydvud04pr1/script1.js
@@ -1,6 +1,4 @@
 class Hospital{
-
-   
      constructor(codHospital, nombreHospital, direccion, telefono, poblacion, codPostal){
         this.codHospital = codHospital;
         this.nombreHospital = nombreHospital;
@@ -8,6 +6,16 @@ class Hospital{
         this.telefono = telefono;
         this.poblacion = poblacion;
         this.codPostal = codPostal;
+    }
+    mostrarDatos(){
+      document.write(`
+      codigo: ${this.codHospital} </br>
+      nombreHospital: ${this.nombreHospital}</br>
+      direccion: ${this.direccion}</br>
+      telefono: ${this.telefono}</br>
+      poblacion: ${this.poblacion}</br>
+      codigo postal: ${this.codPostal}</br>
+      `);
     }
 }
 
@@ -27,4 +35,30 @@ class Medico{
     this.sueldo = sueldo;
     this.hospital = hospital;
    }
+   cambiarSueldo(nuevoSueldo){
+      this.sueldo = nuevoSueldo;
+   }
+   retencionMédico(porcentajeRetencion){
+     retencion =  (porcentajeRetencion * sueldo)/100;
+   }
+   mostrarDatos(){
+      divDatos = document.getElementById("divDatos")
+      divDatos.innerHTML = `<h2>Datos del médico</h2>
+                             codigo: ${this.codMedico}</br>
+                             nombre: ${this.nombre}</br>
+                             apellidos: ${this.apelidos}</br>
+                             dni: ${this.dni}</br>
+                             direccion: ${this.direccion}</br>
+                             telefono: ${this.telefono}</br>
+                             pobacion: ${this.poblacion}</br>
+                             codigo postal: ${this.codPostal}</br>
+                             fecha de nacimiento: ${this.fechaNacimiento}</br>
+                             especialidad: ${this.especialidad}</br>
+                             sueldo: ${this.sueldo}
+                            `;   
+   }
 }
+//codHospital, nombreHospital, direccion, telefono, poblacion, codPostal
+const hospitalRibera = new Hospital("123","Hospital Rivera", "Calle 13", "1254876","Valencia","46009");
+//codMedico, nombre, apellidos, dni,direccion, telefono, poblacion,codPostal, fechaNacimiento, especialidad, sueldo, Hospital
+const medicoDigestivo = new Medico("1236", "Medico", "Traumatólogo", "140526" ,"calle 13","2154878", "valencia", "46009", "18 enero del 95"  );
