b06444e409e9cbaf217de973e81d67e3e5597f18
yoannet <yoadiaval@alu.edu.gva.es>
2024-10-18 20:10:40 +0200

    update

--- DWEC/u4/ydvud04pr1/ejercicio3/script3.js
+++ DWEC/u4/ydvud04pr1/ejercicio3/script3.js
@@ -1,14 +1,31 @@
 const formulario = document.getElementById("form");
+const listEsperaPediatria = document.getElementById("listEsperaPediatria")
+const listEsperaCardio = document.getElementById("listEsperaCardio")
+const listEsperaDerma = document.getElementById("listEsperaDerma")
 
-const pacientesPediatria = [];
-const pacientesDerma = [];
-const pacientesCardio = [];
-const consultas = [pacientesPediatria, pacientesDerma, pacientesCardio];
+let pacientesPediatria = [];
+let pacientesDerma = [];
+let pacientesCardio = [];
+let consultas = [pacientesPediatria, pacientesDerma, pacientesCardio];
+
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
-    consultas[0].push(formulario.elements[0].value)
-    consultas[1].push(formulario.elements[1].value)
-    console.log(consultas)
-    
+    consultas[0].push(formulario.elements[0].value);
+    consultas[1].push(formulario.elements[1].value);   
 })
 
+consultas[0].forEach(element => {
+    listEsperaPediatria.innerHTML += `
+    <li>${element}</li>
+    `;
+});
+
+consultas[1].forEach(element => {
+    listEsperaDerma.innerHTML += `
+    <li>${element}</li>`;
+})
+
+cardio[2].forEach(element => {
+    listEsperaCardio.innerHTML += `
+    <li>${element}</li>`;
+})
