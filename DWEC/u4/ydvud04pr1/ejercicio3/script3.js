1bdc58fe6ff29f2333006f928cb73e9a06675ed8
Yoannet Díaz Valdés <ydiaval@upv.edu.es>
2024-10-18 08:46:46 +0200

    update

--- DWEC/u4/ydvud04pr1/ejercicio3/script3.js
+++ DWEC/u4/ydvud04pr1/ejercicio3/script3.js
@@ -1,12 +1,14 @@
 const formulario = document.getElementById("form");
-const consultas = [[]];
 
+const pacientesPediatria = [];
+const pacientesDerma = [];
+const pacientesCardio = [];
+const consultas = [pacientesPediatria, pacientesDerma, pacientesCardio];
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
-    consultas[0][0].push(formulario.elements[0].value)
-    consultas[0][1].push(formulario.elements[1].value)
+    consultas[0].push(formulario.elements[0].value)
+    consultas[1].push(formulario.elements[1].value)
     console.log(consultas)
-    //console.log(formulario.elements[1].value);
+    
 })
 
-//console.log(consultas)
