bde6bb3e62956c2a79abd7afb609fc3ddd1eab1a
yoadiaval <yoannet93@gmail.com>
2024-10-31 17:34:24 +0100

    update

--- DWEC/u6/ydvud06pr1/ejercicio2/script.js
+++ DWEC/u6/ydvud06pr1/ejercicio2/script.js
@@ -1,3 +1,4 @@
+//listado de imágenes
 const imglist = [
   "./imgs/1.jpg",
   "./imgs/2.jpg",
@@ -5,27 +6,29 @@ const imglist = [
   "./imgs/4.jpg",
   "./imgs/5.jpg",
 ];
+
 let pos = 0;
 const img = document.querySelector("img");
 const positionItems = document.getElementsByClassName("positionItem");
-//timer = setInterval(next, 3000); //ejecuta la funcion next cada 3segundos
+let timer = setInterval(next, 3000); //ejecuta la funcion next cada 3segundos
 
+//funcion que muestra la imagen siguiente
 function next() {
-  img.setAttribute("src", imglist[pos]);
-
-  for (let i = 0; i < positionItems.length; i++) {
-    if (i == pos) {
-      positionItems[pos].style.backgroundColor = "white";
-    } else {
-      positionItems[i].style.backgroundColor = " rgba(255, 255, 255, 0.338)";
-    }
-  }
-
+  cambiarImagen(pos);
   pos == imglist.length - 1 ? (pos = 0) : pos++;
 }
 
+//funcion que muestra la imagen anterior
 function backward(element) {
+  cambiarImagen(pos);
+  pos == 0 ? (pos = imglist.length - 1) : pos--;
+}
+
+//cambia la imagen que se muestra
+function cambiarImagen(pos) {
   img.setAttribute("src", imglist[pos]);
+
+  //resalta en blanco la posición de la imagen en curso y al resto les agrega opacidad
   for (let i = 0; i < positionItems.length; i++) {
     if (i == pos) {
       positionItems[pos].style.backgroundColor = "white";
@@ -33,9 +36,9 @@ function backward(element) {
       positionItems[i].style.backgroundColor = " rgba(255, 255, 255, 0.338)";
     }
   }
-  pos == 0 ? (pos = imglist.length - 1) : pos--;
 }
 
+//imprime en pantalla los puntos que indican la posicion de la imagen
 for (let i = 0; i < imglist.length; i++) {
   document.getElementById(
     "position"
---
+++ DWEC/u6/ydvud06pr1/ejercicio3/script.js
@@ -0,0 +1,61 @@
+const zonaDibujoTable = document.getElementById("zonadibujo_table");
+const paletaTable = document.getElementById("paleta_table");
+
+//--------------------MAQUETAR PALETA DE COLORES-------------------------//
+const celsColorPaleta = new Array(6).fill("<td></td>");
+paletaTable.innerHTML = `
+        <tr>
+            ${celsColorPaleta.join("")}    
+        </tr>
+        <tr>
+          <td id="estado_pincel" colspan="6"></td>
+        </tr>
+`;
+
+//--------------------------MAQUETAR ZONA DE DIBUJO---------------------------------------//
+const cels = new Array(30).fill("<td></td>"); //crea un array de 30 posiciones que completa con las celdas de la tabla
+//genera 30 filas y dentro de cada una de ellas inserta el array concatenado de las 30 celdas.
+for (let index = 0; index < 30; index++) {
+  zonaDibujoTable.innerHTML += `<tr>${cels.join("")}</tr>`;
+}
+
+
+//----------------------------GESTIONAR PALETA-----------------------------------//
+let activeColor; //almacena el color que esté activo.
+
+document
+  .querySelectorAll("#paleta_table tr:nth-child(1) td")
+  .forEach((element) => {
+    element.addEventListener("click", function () {
+      activeColor = getComputedStyle(this).backgroundColor; //se accede al color seleccionado
+     
+    });
+  });
+
+
+//--------------------------CONTROL CLICK DE ACTIVACION Y DESACTIVACION------------------------// 
+let nclick = 0;
+document.getElementById("zonadibujo").addEventListener("click", function () { //escucha al evento click dentro de la zona de dibujo
+  nclick == 1 ? (nclick = 0) : nclick++; //alterna nclick entre 0 y 1
+  console.log(nclick);
+});
+
+//------------------------------GESTIONAR ESTADO DEL PINCEL--------------------------------------// 
+setInterval(revisarEstadoPincel, 100); //revisa el estado del pincel y lo actualiza cada 100ms
+
+function revisarEstadoPincel() {
+  document.getElementById("estado_pincel").innerHTML = `${
+    nclick === 0 ? "PINCEL DESACTIVADO" : "PINCEL ACTIVADO"
+  }`;
+}
+
+
+//---------------------------------GESTIONAR ZONA DE DIBUJO----------------------------------------------//
+document.querySelectorAll("#zonadibujo_table td").forEach((element) => {
+  element.addEventListener("mouseover", function () {
+    if (nclick == 1) {
+      this.style.backgroundColor = activeColor;
+    }
+  });
+});
+
