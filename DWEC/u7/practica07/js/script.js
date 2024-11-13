aaad69eb0013d48396241d70530262419820d91f
yoadiaval <yoannet93@gmail.com>
2024-11-13 12:42:02 +0100

    update

--- DWEC/u7/practica07/js/script.js
+++ DWEC/u7/practica07/js/script.js
@@ -2,7 +2,11 @@ const table = document.getElementById("list");
 const formulario = document.getElementById("formulario");
 const formModificar = document.getElementById("formModificar");
 
-//Obtengo los datos al cargar la página
+const legend = document.querySelector("legend");
+var modificando = false;
+var creando = false;
+
+//Obtengo los datos al cargar la página y actualizo el DOM
 window.onload = function () {
   getData();
   //oculta los formularios al cargar la página
@@ -11,24 +15,90 @@ window.onload = function () {
   });
 };
 
-
-//-------------------LÓGICA OCULTAR Y MOSTAR SECCIONES--------------------// 
+//------------------MANEJO DE EVENTOS EN LA PÁGINA---------------------------------//
+//Agrega evento al button que añade empleados
 document.getElementById("add").addEventListener("click", function () {
+  creando = true;
+  legend.innerHTML = "Inserte los datos de nuevo empleado";
   document.getElementById("section1").classList.add("ocultar");
   document.getElementById("section2").classList.remove("ocultar");
 });
-
-document.getElementById("cancelar1").addEventListener("click", function () {
+//Agrega eventos al button cancelar
+document.getElementById("cancelar").addEventListener("click", function () {
+  //Pongo a false las acciones para evitar que se afecte la lógica de envío de datos del formulario.
+  modificando = false;
+  creando = false;
+  document.getElementById("section1").classList.remove("ocultar");
   document.getElementById("section2").classList.add("ocultar");
-  document.getElementById("section1").classList.remove("ocultar");
+});
+//function que maneja el evento de cada uno de los buttons de modificar
+function prepararModificado(element) {
+  var peticion = new XMLHttpRequest();
+  peticion.onreadystatechange = function () {
+    if (
+      peticion.readyState == 4 &&
+      (peticion.status === 200 ||
+        peticion.status === 201 ||
+        peticion.status === 204)
+    ) {
+      modificando = true;
+      //Agrega los valores del empleado seleccionado a los imputs correspondientes
+      empleado = JSON.parse(peticion.responseText)[0];
+      formulario[1].value = empleado.nombre;
+      formulario[2].value = empleado.edad;
+      formulario[3].value = empleado.cargo;
+      formulario[4].checked = empleado.contratado == 1 ? true : false;
+      formulario[5].value = empleado.id;
+      //muestro el fomulario de modificacion de empleados
+      legend.innerHTML = "Modifique los datos necesarios";
+      document.getElementById("section1").classList.add("ocultar");
+      document.getElementById("section2").classList.remove("ocultar");
+    }
+  };
+  peticion.open(
+    "GET",
+    `http://test-api.jtarrega.es/api/empleados/${element.classList[0]}`,
+    true
+  );
+  peticion.setRequestHeader("Content-Type", "application/json");
+  peticion.send();
+}
+
+//------------------- EVENTO SUBMIT FORMULARIO-------------------------//
+//Formulario que agrega y modifica empleados
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //Recojo la información del formulario
+  var datosJson = JSON.stringify({
+    nombre: formulario[1].value,
+    edad: formulario[2].value,
+    cargo: formulario[3].value,
+    contratado: formulario[4].checked ? 1 : 0,
+    created_at: "",
+    updated_at: "",
+  });
+
+  //llamo a la función que crea el formulario o a la que lo modifica en 
+  //función del estado de "creando" y "modificando".
+  switch (true) {
+    case creando:
+      creando = false;
+      actionPost(datosJson); 
+      formulario.reset();
+      break;
+    case modificando:
+      console.log(formulario[4]);
+      var id = formulario[5].value;
+      actionPut(datosJson, id);
+      formulario.reset();
+      modificando = false;
+      break;
+    default:
+      break;
+  }
 });
 
-document.getElementById("cancelar2").addEventListener("click", function () {
-  document.getElementById("section3").classList.add("ocultar");
-  document.getElementById("section1").classList.remove("ocultar");
-});
-
-//------------------------FUNCIONES CRUD------------------------------//
+//---------------------------FUNCIONES CRUD---------------------------//
 //Obtiene los datos y actualiza el DOM
 function getData() {
   var peticion = new XMLHttpRequest();
@@ -57,9 +127,11 @@ function getData() {
                                                         <td>${edadEmpleado}</td>
                                                         <td>${cargoEmpleado}</td>
                                                         <td>${estadoContratado}</td>
-                                                        <td class="acciones"><button class="${idEmpleado}" onclick="modificarEmpleado(this)">Modificar</button><button onclick="actionDelete(this)" class="${idEmpleado} btnBorrar">Borar</button></td>
+                                                        <td class="acciones"><button class="${idEmpleado}" onclick="prepararModificado(this)">Modificar</button><button onclick="actionDelete(this)" class="${idEmpleado} btnBorrar">Borar</button></td>
                                                       </tr>`;
       }
+      document.getElementById("section1").classList.remove("ocultar");
+      document.getElementById("section2").classList.add("ocultar");
     }
   };
 
@@ -68,50 +140,35 @@ function getData() {
   peticion.setRequestHeader("Content-Type", "application/json");
   peticion.send();
 }
-//agrega al empleado pasado por parámetro
-function actionPost(nuevoEmpleado) {
+//Crea nuevo empleado
+function actionPost(datos) {
   var peticion = new XMLHttpRequest();
   peticion.onreadystatechange = function () {
     if (peticion.readyState == 4 && peticion.status == 201) {
-      console.log("Empleado agregado exitosamente");
       getData();
     }
   };
   peticion.open("POST", "http://test-api.jtarrega.es/api/empleados", true);
   peticion.setRequestHeader("Content-Type", "application/json");
-  peticion.send(nuevoEmpleado);
-
-  
+  peticion.send(datos);
 }
-//Mofifica el empleado a partir de los datos recogidos del formModificar
-function actionPut() {
+//Mofifica el empleado
+function actionPut(datos, id) {
   var peticion = new XMLHttpRequest();
   peticion.onreadystatechange = function () {
-    if (peticion.readyState == 4 || peticion.status === 201) {     
+    if (peticion.readyState == 4 || peticion.status === 201) {
       getData();
-      document.getElementById("section3").classList.add("ocultar");
-      document.getElementById("section1").classList.remove("ocultar");
     }
   };
   peticion.open(
     "PUT",
-    `http://test-api.jtarrega.es/api/empleados/${formModificar[5].value}`, //recoge el id en el campo oculto de este formulario
+    `http://test-api.jtarrega.es/api/empleados/${id}`, //recoge el id en el campo oculto de este formulario
     true
   );
   peticion.setRequestHeader("Content-Type", "application/json");
-  var datosJson = JSON.stringify({
-    nombre: formModificar[1].value,
-    edad: formModificar[2].value,
-    cargo: formModificar[3].value,
-    contratado: formModificar[4].checked ? 1 : 0,
-    created_at: "",
-    updated_at: "",
-  });
-
-  peticion.send(datosJson);
-  
+  peticion.send(datos);
 }
-//elimina al empleado a partir del id que se pasa en la clase en la clase del elemento que lanza la accion
+//elimina al empleado a partir del id que se pasa en la clase en la clase del elemento que lanza la acción
 function actionDelete(element) {
 
  var peticion = new XMLHttpRequest();
@@ -121,7 +178,6 @@ function actionDelete(element) {
      peticion.status === 201 ||
      peticion.status === 204)
    ) {
-     console.log("Empleado eliminado exitosamente");
      getData();
    } 
  };
@@ -135,35 +191,12 @@ function actionDelete(element) {
 
 }
 
+
+//agrega al empleado pasado por parámetro
+/*
+
 //--------------------------OTRAS FUNCIONES--------------------------//
-function modificarEmpleado(element) {
-  var peticion = new XMLHttpRequest();
-  peticion.onreadystatechange = function () {
-    if (peticion.readyState == 4 && (
-      peticion.status === 200 ||
-      peticion.status === 201 ||
-      peticion.status === 204)
-    ) {
-      //Agrega los valores del empleado seleccionado a los imputs correspondientes
-      empleado = JSON.parse(peticion.responseText)[0];
-      formModificar[1].value = empleado.nombre;
-      formModificar[2].value = empleado.edad;
-      formModificar[3].value = empleado.cargo;
-      formModificar[4].checked = empleado.contratado == 1 ? true : false;
-      formModificar[5].value = empleado.id;
-      //muestro el fomulario de modificacion de empleados
-      document.getElementById("section1").classList.add("ocultar");
-      document.getElementById("section3").classList.remove("ocultar");
-    }
-  };
-  peticion.open(
-    "GET",
-    `http://test-api.jtarrega.es/api/empleados/${element.classList[0]}`,
-    true
-  );
-  peticion.setRequestHeader("Content-Type", "application/json");
-  peticion.send();
-}
+
 
 //Obtiene la cantidad de elementos de la base de datos para evitar eliminar el último elemento
 function obtenerLongitud(){
@@ -187,20 +220,7 @@ function obtenerLongitud(){
 }
 
 //---------------CAPTURA DE EVENTOS DE LOS FORMULARIOS------------------------//
-//Formulario nuevo empleado
-formulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var datosJson = JSON.stringify({
-    nombre: formulario[1].value,
-    edad: formulario[2].value,
-    cargo: formulario[3].value,
-    contratado: formulario[4].checked ? 1 : 0,
-    created_at: "",
-    updated_at: "",
-  });
-  actionPost(datosJson);
-  formulario.reset();
-});
+
 
 //formulario para modificar empleado
 formModificar.addEventListener("submit", function (event) {
@@ -208,3 +228,4 @@ formModificar.addEventListener("submit", function (event) {
   actionPut();
   this.reset();
 });
+*/
