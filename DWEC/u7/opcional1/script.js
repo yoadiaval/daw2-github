8ab7ba355875e6dc19b8f10f075bdc92e458bfa3
yoannet <yoadiaval@alu.edu.gva.es>
2024-11-25 15:55:08 +0100

    update

--- DWEC/u7/opcional1/script.js
+++ DWEC/u7/opcional1/script.js
@@ -1,12 +1,60 @@
 formNuevoPaciente = document.getElementById("formNuevoPaciente");
 btnBorrarTodo = document.getElementById("btn-borrar-todo");
+listaTable = document.getElementById("lista-pacientes-editar");
 
 // Event listener para el formulario
+window.onload = function(){
+  listarpacientes();
+}
 
+//captura del formulario.
 formNuevoPaciente.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(this.elements[2].value);
-  text = "2024-11-25T12:36";
-  arr = text.split("T");
-  console.log(arr);
+  dateTime = this.elements[2].value.split("T");
+  date =  dateTime[0];
+  hora = dateTime[1];
+  datosJson = JSON.stringify(
+    {
+      nombre: this.elements[0].value,
+      apellidos: this.elements[1].value,
+      fecha: date,
+      hora: hora
+    }
+  );
+
+   sessionStorage.setItem(this.elements[0].value,datosJson); //la clave es el  nombre de la persona
+   this.reset();
+   listarpacientes();
 });
+
+//borrado del sessionStorage
+btnBorrarTodo.addEventListener("click", function (event) {
+  sessionStorage.clear();
+  listarpacientes();
+});
+
+//listar pacientes introducidos
+function listarpacientes(){
+  
+    listaTable.innerHTML = `<tr> 
+    <td>Nombre</td>
+    <td>Apellidos</td>
+    <td>Fecha</td>
+    <td>Hora</td>
+</tr>
+`;
+for (var i = 1; i <= sessionStorage.length; i++) {
+paciente = sessionStorage.key(i);
+pacienteObj = JSON.parse(sessionStorage[paciente]);
+
+listaTable.innerHTML += `<tr>
+<td>${pacienteObj.nombre}</td>
+<td>${pacienteObj.apellidos}</td>
+<td>${pacienteObj.fecha}</td>
+<td>${pacienteObj.hora}</td> 
+</tr>`;
+}
+
+  
+  
+}
