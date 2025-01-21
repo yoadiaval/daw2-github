ef0714d187a878b5ad8f9af7407aa5dc2d195d9d
yoadiaval <yoannet93@gmail.com>
2025-01-21 09:17:51 +0100

    update

--- DWES/proyectos/APIs/api.php
+++ DWES/proyectos/APIs/api.php
@@ -1,38 +1,50 @@
-<?php 
+<?php
 header('Content-Type: application/json');
 include 'db.php';
 include "./producto.php";
 
 // Obtener la acción de la URL (GET, POST, PUT, DELETE)
 $request_method = $_SERVER['REQUEST_METHOD'];
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); /*La combinacin de parse_url y PHP_URL_PATH devuelve solo el end point /usuarios/5 que en principio hará referencia a la tabla y el id de la base de datos*/ 
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); /*La combinacin de parse_url y PHP_URL_PATH devuelve solo el end point /usuarios/5 que en principio hará referencia a la tabla y el id de la base de datos*/
 $uri_parts = explode('/', $uri); /*para un uri /usuarios/2 -> [ '' , usuarios , 2]*/
 
 // El ID del recurso (si es necesario) lo tomamos de la URL
-$id = isset($uri_parts[2]) ? (int)$uri_parts[2] : null; /*Busco el id en la posición 2*/ 
+$id = isset($uri_parts[2]) ? (int) $uri_parts[2] : null; /*Busco el id en la posición 2*/
 
-switch ($request_method) {
-    case 'GET':
-            Producto::getProductos();
-            break;
-    case 'POST':
-            Producto::postProducto();
-            break;
-    case 'PUT':
-            Producto::putProducto();
-            break;
-    case 'PUT':
-            Producto::patchProducto();
-            break;
-    case 'DELETE':
-           Producto::DeleteProducto();
-            break;
-    default:
-        # code...
-        break;
+if ($request_method == "GET") {
+        if ($id !== null) {
+                $condicion = "id = {$id}";
+                $productos = Producto::getProductos($condicion);
+
+        } else {
+                $productos = Producto::getProductos();
+        }
+
+        if ($respuesta) {
+
+                return json_encode($productos);
+        }
+
 }
 
+/*$data = json_decode(file_get_contents("php://input"), true);
+switch ($request_method) {
+        case 'POST':
+                Producto::postProducto($data);
+                break;
+        case 'PUT':
+                 Producto::putProducto($data);
+                break;
+        case 'PATCH':
+                Producto::patchProducto($data);
+                break;
+        case 'DELETE':
+                Producto::DeleteProducto($data);
+                break;
+        default:
 
+                break;
+}*/
 
 
 ?>
--- DWES/proyectos/APIs/producto.php
+++ DWES/proyectos/APIs/producto.php
@@ -1,12 +1,20 @@
 <?php
-class Producto{
-    public static getProductos($condicion = 1){
-        
+include_once "./db.php";
+class Producto
+{
+    public static function getProductos($condicion = 1)
+    {
+        $conexion = ConexionDB::conectar();
+        $sql = "SELECT * FORM prodctos where $condicion;";
+        $respuesta = $conexion->query($sql);
+        if ($respuesta) {
+            return $respuesta->fetch_all(MYSQLI_ASSOC);
+        }
     }
-    public static postProducto(){}
-    public static putProducto(){}
-    public static patchProducto(){}
-    public static deleteProducto(){}
+    /*  public static function postProducto($data){}
+     public static function putProducto($data){}
+     public static function patchProducto($data){}
+     public static function deleteProducto($data){} */
 }
 
 ?>
